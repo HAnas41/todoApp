4705cc566c044baeba7039951ca8e78c539688cd
HAnas41 <muhammadanaas.334@gmail.com>
2024-09-02 17:36:03 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let todoQuestions = await inquirer.prompt([
+        {
+            name: "firstQuestion",
+            type: "input",
+            message: "What would you like to add in your todo app?"
+        },
+        {
+            name: 'secondQuestion',
+            type: 'confirm',
+            message: 'What would you like to add more in todo app?',
+            default: true
+        }
+    ]);
+    todos.push(todoQuestions.firstQuestion);
+    console.log(todos);
+    condition = todoQuestions.firstQuestion;
+}
